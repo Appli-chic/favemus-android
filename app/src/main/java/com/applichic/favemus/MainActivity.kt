e460cf57bae77a0644f183f57ab5b29cbda2defc
applichic <applichic@gmail.com>
2020-04-10 19:12:52 +0200

    Create navigation with bottom bar.

--- app/src/main/java/com/applichic/favemus/MainActivity.kt
+++ app/src/main/java/com/applichic/favemus/MainActivity.kt
@@ -2,6 +2,10 @@ package com.applichic.favemus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import com.applichic.favemus.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +16,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Set first fragment
+        val homeFragment = HomeFragment.newInstance()
+        openFragment(homeFragment)
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_search -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_chat -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
     }
 }
